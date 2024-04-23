7996d74646384b5d0ac75fd8aaa5d560eca00485
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-23 21:02:10 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // write code here
+function checkPermission(role, isAdminAction, isNotAdminAction) {
+  if (role === "ADMIN") {
+    isAdminAction();
+  } else {
+    isNotAdminAction();
+  }
+}
+
+function isAdminAction() {
+  alert("ACCESS GRANTED");
+  console.log("ACCESS GRANTED");
+}
+
+function isNotAdminAction() {
+  alert("ACCESS DENIED");
+  console.log("ACCESS DENIED");
+}
+
+let role = prompt("Enter role (ADMIN or USER):");
+
+checkPermission(role, isAdminAction, isNotAdminAction);
